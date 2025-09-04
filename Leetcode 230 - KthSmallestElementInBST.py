cb1777078e27b0380af527a88f1ae7624c80151a
kct26 <dangmkhoa2522@gmail.com>
2025-09-04 13:49:41 +0700

    Create Leetcode 230 - KthSmallestElementInBST.py

---
+++ Leetcode 230 - KthSmallestElementInBST.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        ans = []
+        res = None
+        count = 0
+        def dfs (root):
+            nonlocal res, count
+            if not root or res is not None:
+                return 
+            dfs (root.left)
+            count += 1
+            if count == k:
+                res = root.val
+                return
+            dfs (root.right)
+        dfs(root)
+        return res
+                
+            
+        
