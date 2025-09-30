da0ff723460eb51eece8913e770a6c68a2dd81c8
kct26 <dangmkhoa2522@gmail.com>
2025-09-30 11:17:16 +0700

    Create leetcode 2221

---
+++ Leetcode 2221 - TriangularSumOfAnArray.py
@@ -0,0 +1,8 @@
+import math
+class Solution:
+    def triangularSum(self, nums: List[int]) -> int:
+        n = len(nums)
+        result = 0
+        for i in range(n):
+            result = (result + comb(n - 1, i) * nums[i]) % 10
+        return result
