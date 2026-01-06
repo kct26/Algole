834307f46b222bb1902184ffcbae9edbfda1fc2e
kct26 <dangmkhoa2522@gmail.com>
2026-01-06 23:41:36 +0700

    Create Leetcode 236

---
+++ Leetcode 236 - LowestCommonAncestorOfBinaryTree.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        def dfs(root):
+            if not root:
+                return None
+            if root == p or root == q:
+                return root
+            l = dfs(root.left)
+            r = dfs(root.right)
+            if l is not None and r is not None:
+                return root
+            if l is not None:
+                return l
+            if r is not None:
+                return r
+            return None
+        return dfs(root)
